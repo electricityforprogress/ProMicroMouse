ec973088cc34aab7621c259b70ded97adb547d08
Nickeytheblade <93241171+Nickeytheblade@users.noreply.github.com>
2022-10-17 12:15:03 -0700

    Update main.cpp

--- Platfromio/src/main.cpp
+++ Platfromio/src/main.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 #include <Mouse.h>
 
-int distance = 5; //how far to 'wiggle'
+int distance = 1; //how far to 'wiggle'
 int wiggleTime = 1; //milliseconds to perform wiggle displacement
-int waitTime = 20; //delay between wiggles
+int waitTime = 90; //delay between wiggles
 
 void setup() { 
   Serial.begin(9600);
